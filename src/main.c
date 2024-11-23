21c92db251c21f2dab8b5f05d944febb717f281a
noonacedia <ruyearn@gmail.com>
2024-11-23 20:11:18 +0400

    [develop] Backup push

--- src/main.c
+++ src/main.c
@@ -1,15 +1,28 @@
 #include "raylib.h"
+#include "rlgl.h"
 
 int main(void) {
   const int screenWidth = 800;
   const int screenHeight = 600;
   const char *screenTitle = "Conway's Game of life";
-  Color slightGreenColor = {.r = 41, .g = 196, .b = 60, .a = 1};
+  // Color slightGreenColor = {.r = 206, .g = 169, .b = 188, .a = 1};
 
   InitWindow(screenWidth, screenHeight, screenTitle);
+  // Camera2D camera = {0};
+  // camera.zoom = 1.0f;
+  // int zoomMode = 0;
+
+  SetTargetFPS(65);
+
   while (!WindowShouldClose()) {
     BeginDrawing();
-    ClearBackground(slightGreenColor);
+    ClearBackground(RAYWHITE);
+    // BeginMode2D(camera);
+    rlPushMatrix();
+    rlTranslatef(0, 25 * 50, 0);
+    rlRotatef(90, 1, 0, 0);
+    DrawGrid(100, 50);
+    rlPopMatrix();
     EndDrawing();
   }
 
