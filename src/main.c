0a2f83c0ee6aad0d3bd210ebce58464e308eab14
noonacedia <ruyearn@gmail.com>
2024-12-21 14:22:22 +0400

    [develop] Add scale changing and beta mouse scrolling

--- src/main.c
+++ src/main.c
@@ -1,23 +1,50 @@
 #include "raylib.h"
+#include "raymath.h"
 #include "rlgl.h"
+
+void zoomMouseWheel(Camera2D *camera) {
+  float wheel = GetMouseWheelMove();
+  if (wheel != 0) {
+    Vector2 mouseWordPos = GetScreenToWorld2D(GetMousePosition(), *camera);
+    camera->offset = GetMousePosition();
+    camera->target = mouseWordPos;
+    float scaleFactor = 1.0f + (0.25 * fabs(wheel));
+    if (wheel < 0)
+      scaleFactor = 1.0f / scaleFactor;
+    camera->zoom = Clamp(camera->zoom * scaleFactor, 0.125f, 64.0f);
+  }
+}
+
+void moveCameraWithMouse(Camera2D *camera) {
+  if (IsMouseButtonDown(0)) {
+    Vector2 newVectorOffset = {
+        GetMouseX(),
+        GetMouseY(),
+    };
+    camera->offset = newVectorOffset;
+    camera->target = newVectorOffset;
+  }
+}
 
 int main(void) {
   const int screenWidth = 800;
   const int screenHeight = 600;
   const char *screenTitle = "Conway's Game of life";
-  // Color slightGreenColor = {.r = 206, .g = 169, .b = 188, .a = 1};
+  const int fps = 100;
 
   InitWindow(screenWidth, screenHeight, screenTitle);
-  // Camera2D camera = {0};
-  // camera.zoom = 1.0f;
-  // int zoomMode = 0;
-
-  SetTargetFPS(65);
+  Vector2 cameraVectorOffset = {0, 0};
+  Vector2 cameraVectorTarget = {0, 0};
+  Camera2D camera = {cameraVectorOffset};
+  camera.zoom = 1.0f;
+  SetTargetFPS(fps);
 
   while (!WindowShouldClose()) {
+    zoomMouseWheel(&camera);
+    moveCameraWithMouse(&camera);
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    // BeginMode2D(camera);
+    BeginMode2D(camera);
     rlPushMatrix();
     rlTranslatef(0, 25 * 50, 0);
     rlRotatef(90, 1, 0, 0);
